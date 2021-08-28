004fcc9be8e786406d4a296e1e06c324c88c01b7
JunqiYe <caojinjin008@hotmail.com>
2021-08-27 21:12:59 -0700

    lcd prototype init

---
+++ LCD_prototype/Core/Src/LCD16x2_cfg.c
@@ -0,0 +1,23 @@
+#include "LCD16x2.h"
+
+const LCD16x2_CfgType LCD16x2_CfgParam =
+{
+    GPIOB,
+    GPIO_PIN_2,
+    GPIO_PIN_3,
+    GPIO_PIN_4,
+    GPIO_PIN_5,
+    GPIO_PIN_6,
+    GPIO_PIN_13,
+    50
+};
+
+
+/*
+ *  uint16_t D4_PIN;
+    uint16_t D5_PIN;
+    uint16_t D6_PIN;
+    uint16_t D7_PIN;
+    uint16_t EN_PIN;
+    uint16_t RS_PIN;
+ */
